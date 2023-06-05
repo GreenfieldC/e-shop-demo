00ab19b784e4d4977d9efb6ce9142267686a8a7d
GreenfieldC <101995422+GreenfieldC@users.noreply.github.com>
2023-06-05 21:05:34 +0200

    card to see

--- src/app/products/product-item/product-item.component.ts
+++ src/app/products/product-item/product-item.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
-  styleUrls: ['./product-item.component.scss']
+  styleUrls: ['./product-item.component.scss'],
 })
 export class ProductItemComponent {
-
+  @Input() product: any;
 }
--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiService } from 'src/app/shared/services/api.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.scss']
+  styleUrls: ['./product-list.component.scss'],
 })
 export class ProductListComponent {
+  products$: Observable<any>;
 
+  constructor(private apiService: ApiService) {
+    this.products$ = this.apiService.getProducts;
+    this.apiService.getProducts.subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
