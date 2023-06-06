4d6b1bcfd2fbde459f42c4fb78f51f6d9d86d4c4
GreenfieldC <101995422+GreenfieldC@users.noreply.github.com>
2023-06-06 13:24:01 +0200

    basics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProductsModule } from './products/products.module';
 
 const routes: Routes = [
   {
