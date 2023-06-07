fd0dd3fc883196a599c7aa81fdd91a584994379b
GreenfieldC <101995422+GreenfieldC@users.noreply.github.com>
2023-06-07 16:58:37 +0200

    mat-card started, does not work, import?

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -14,6 +14,7 @@ import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
 import { RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
+
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
