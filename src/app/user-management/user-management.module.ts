193a6ba287c653b2d9e6eec40445027ec620c319
GreenfieldC <101995422+GreenfieldC@users.noreply.github.com>
2023-06-07 20:37:25 +0200

    import sharedModule

--- src/app/user-management/user-management.module.ts
+++ src/app/user-management/user-management.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
 
 import { UserManagementRoutingModule } from './user-management-routing.module';
 import { LoginPageComponent } from './login-page/login-page.component';
 
-
 @NgModule({
-  declarations: [
-    LoginPageComponent
-  ],
-  imports: [
-    CommonModule,
-    UserManagementRoutingModule
-  ]
+	declarations: [LoginPageComponent],
+	imports: [CommonModule, UserManagementRoutingModule, SharedModule],
 })
-export class UserManagementModule { }
+export class UserManagementModule {}
