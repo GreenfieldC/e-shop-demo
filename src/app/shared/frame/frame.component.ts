528f39a43d87b8b76f8f0d8f196ff7df12857c9d
MoritzGeorgy95 <104759604+MoritzGeorgy95@users.noreply.github.com>
2023-06-07 14:16:01 +0200

    finished header icons

--- src/app/shared/frame/frame.component.ts
+++ src/app/shared/frame/frame.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-frame',
-  templateUrl: './frame.component.html',
-  styleUrls: ['./frame.component.scss']
+	selector: 'app-frame',
+	templateUrl: './frame.component.html',
+	styleUrls: ['./frame.component.scss'],
 })
 export class FrameComponent {
-
+	dropDownOpen: boolean = false;
+	selectedCurrency: string = 'EUR';
 }
