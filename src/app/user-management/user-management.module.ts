699cf8eb5e4e08d9aeb0022480f7d8e041c2ad83
GreenfieldC <101995422+GreenfieldC@users.noreply.github.com>
2023-06-07 20:05:30 +0200

    restructure

--- src/app/user-management/user-management.module.ts
+++ src/app/user-management/user-management.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { SharedModule } from '../shared/shared.module';
+
 import { UserManagementRoutingModule } from './user-management-routing.module';
 import { LoginPageComponent } from './login-page/login-page.component';
 
