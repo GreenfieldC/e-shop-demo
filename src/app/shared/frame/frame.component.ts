c28d1981139e449158a481e4d3da57abeb1b1a4f
MoritzGeorgy95 <104759604+MoritzGeorgy95@users.noreply.github.com>
2023-06-07 15:32:02 +0200

    dropdown menu closing when user clicks next to it

--- src/app/shared/frame/frame.component.ts
+++ src/app/shared/frame/frame.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
 	selector: 'app-frame',
@@ -8,4 +8,31 @@ import { Component } from '@angular/core';
 export class FrameComponent {
 	dropDownOpen: boolean = false;
 	selectedCurrency: string = 'EUR';
+	clickCounter: number = 0;
+
+	@ViewChild('dropdown') el: any;
+	// Close the dropdown menu when a user clicks outside of it
+	@HostListener('document:click', ['$event'])
+	onDocumentClick(event: MouseEvent): void {
+		if (this.dropDownOpen) {
+			// Check if the clicked element is outside the dropdown menu
+			const clickedElement = event.target as HTMLElement;
+			const dropdownMenu = this.el.nativeElement;
+
+			if (
+				dropdownMenu &&
+				!dropdownMenu.contains(clickedElement) &&
+				this.clickCounter === 0
+			) {
+				this.clickCounter++;
+			} else if (
+				dropdownMenu &&
+				!dropdownMenu.contains(clickedElement) &&
+				this.clickCounter > 0
+			) {
+				this.dropDownOpen = false;
+				this.clickCounter = 0;
+			}
+		}
+	}
 }
