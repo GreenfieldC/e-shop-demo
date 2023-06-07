e00def9ecef75d2cad28d0b5eba7068242806f49
MoritzGeorgy95 <104759604+MoritzGeorgy95@users.noreply.github.com>
2023-06-07 13:25:35 +0200

    created header & fixed matIconIssue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { ApiService } from './shared/services/api.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'shop-demo';
+	title = 'shop-demo';
 }
