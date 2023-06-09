8759fc31f343067a2b86c62aecb1ef30ab0c8a70
MoritzGeorgy95 <104759604+MoritzGeorgy95@users.noreply.github.com>
2023-06-09 15:47:34 +0200

    started with product detail view logic

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -3,17 +3,19 @@ import { Observable } from 'rxjs';
 import { ApiService } from 'src/app/shared/services/api.service';
 
 @Component({
-  selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.scss'],
+	selector: 'app-product-list',
+	templateUrl: './product-list.component.html',
+	styleUrls: ['./product-list.component.scss'],
 })
 export class ProductListComponent {
-  products$: Observable<any>;
+	products$: Observable<any>;
 
-  constructor(private apiService: ApiService) {
-    this.products$ = this.apiService.getProducts;
-    this.apiService.getProducts.subscribe((data) => {
-      console.log(data);
-    });
-  }
+	constructor(private apiService: ApiService) {
+		this.products$ = this.apiService.getProducts;
+		this.apiService.getProducts.subscribe((data) => {
+			console.log(data);
+		});
+	}
+
+	openDetailView(product: any) {}
 }
