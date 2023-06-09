46f480a098cdbeef8784d45a0d5298f41e44d24e
MoritzGeorgy95 <104759604+MoritzGeorgy95@users.noreply.github.com>
2023-06-09 11:28:17 +0200

    mat card test

--- src/app/products/product-details/product-details.component.ts
+++ src/app/products/product-details/product-details.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @Component({
-  selector: 'app-product-details',
-  templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.scss']
+	selector: 'app-product-details',
+	templateUrl: './product-details.component.html',
+	styleUrls: ['./product-details.component.scss'],
 })
-export class ProductDetailsComponent {
-
-}
+export class ProductDetailsComponent {}
--- src/app/shared/frame/frame.component.ts
+++ src/app/shared/frame/frame.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, ElementRef, ViewChild } from '@angular/core';
+import { Component, HostListener, ViewChild } from '@angular/core';
 
 @Component({
 	selector: 'app-frame',
