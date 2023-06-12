327f798531e81c247853bce70035bc3e72e3b065
MoritzGeorgy95 <104759604+MoritzGeorgy95@users.noreply.github.com>
2023-06-12 17:26:02 +0200

    firebase storage complete

--- src/app/shared/services/shopping-basket.service.ts
+++ src/app/shared/services/shopping-basket.service.ts
@@ -17,14 +17,26 @@ import { Observable } from 'rxjs';
 	providedIn: 'root',
 })
 export class ShoppingBasketService {
+	//document reference in Firestore
 	private cartDocRef: DocumentReference;
+
+	//products array that get displayed in UI
 	products: Array<any> = [];
+
+	//total price
 	totalPrice: number = 0;
 
 	constructor(private firestore: Firestore) {
+		//asssignment of document reference in Firestore
 		this.cartDocRef = doc(this.firestore, 'user_guest/cart');
+
+		//built-in observable creator that establishes real time listener of database
 		onSnapshot(this.cartDocRef, (doc) => {
 			this.products = doc.data()?.['products'] || [];
+
+			this.products.forEach((product) => {
+				this.totalPrice += product.price;
+			});
 		});
 	}
 
