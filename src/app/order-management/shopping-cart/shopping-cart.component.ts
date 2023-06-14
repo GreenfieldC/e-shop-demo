31f6a81a7120897e15674c9dc890f464c783c258
MoritzGeorgy95 <104759604+MoritzGeorgy95@users.noreply.github.com>
2023-06-14 14:38:38 +0200

    finished shopping cart logic

--- src/app/order-management/shopping-cart/shopping-cart.component.ts
+++ src/app/order-management/shopping-cart/shopping-cart.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ShoppingBasketService } from 'src/app/shared/services/shopping-basket.service';
+import { ExchangeRateService } from 'src/app/shared/services/exchange-rate.service';
 
 @Component({
 	selector: 'app-shopping-cart',
@@ -7,7 +8,25 @@ import { ShoppingBasketService } from 'src/app/shared/services/shopping-basket.s
 	styleUrls: ['./shopping-cart.component.scss'],
 })
 export class ShoppingCartComponent {
-	constructor(public shoppingCartService: ShoppingBasketService) {}
+	constructor(
+		public shoppingCartService: ShoppingBasketService,
+		public exchangeRateService: ExchangeRateService
+	) {}
 
 	panelOpenState: boolean = false;
+
+	incrementQuantity(i: number, event: Event) {
+		event.stopPropagation();
+		this.shoppingCartService.products[i].quantity += 1;
+	}
+
+	decreaseQuantity(i: number, event: Event) {
+		event.stopPropagation();
+
+		if (this.shoppingCartService.products[i].quantity === 1) {
+			this.shoppingCartService.products.splice(i, 1);
+		} else {
+			this.shoppingCartService.products[i].quantity -= 1;
+		}
+	}
 }
