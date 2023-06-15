efea0953c69b406550036e0a3f9f5c1148b170d2
GreenfieldC <101995422+GreenfieldC@users.noreply.github.com>
2023-06-15 13:28:31 +0200

    searchBar, style upper part

--- src/app/shared/search-bar/search-bar.component.ts
+++ src/app/shared/search-bar/search-bar.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../services/api.service';
+import { ProductItemComponent } from 'src/app/products/product-item/product-item.component';
 
 @Component({
-  selector: 'app-search-bar',
-  templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.scss']
+	selector: 'app-search-bar',
+	templateUrl: './search-bar.component.html',
+	styleUrls: ['./search-bar.component.scss'],
 })
 export class SearchBarComponent {
+	searchTerm: string;
+	products: Array<any>;
 
+	constructor(private apiService: ApiService) {
+		this.apiService.getProducts.subscribe((data) => {
+			/* 	data.forEach((obj: any) => {
+				obj.quantity = 1;
+				obj.size = '';
+			}); */
+			this.products = data;
+			console.log(this.products);
+		});
+	}
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,26 +1,27 @@
 /* hier kommt alles rein, was auch woanders gebraucht wird.
 muss in jedes modul importiert werden */
 
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
 /* Material Design */
-import { MatButtonModule } from '@angular/material/button';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
-import { RouterModule } from '@angular/router';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
 import { FrameComponent } from './frame/frame.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
-import { MatDialogModule } from '@angular/material/dialog';
 
 const components = [FrameComponent, SearchBarComponent];
 const modules = [
@@ -39,6 +40,7 @@ const modules = [
 	MatSnackBarModule,
 	MatDialogModule,
 	MatExpansionModule,
+	FormsModule,
 ];
 
 @NgModule({
