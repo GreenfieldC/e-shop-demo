f92d49f26703fd27dab2bc7d05d8f905fa792dbe
MoritzGeorgy95 <104759604+MoritzGeorgy95@users.noreply.github.com>
2023-06-27 14:53:24 +0200

    finished database integration for orders

--- src/app/order-management/dialog-payment/dialog-payment.component.ts
+++ src/app/order-management/dialog-payment/dialog-payment.component.ts
@@ -1,6 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 import { TitleCasePipe } from '@angular/common';
+import { OrderHistoryService } from 'src/app/shared/services/order-history.service';
+import { ShoppingBasketService } from 'src/app/shared/services/shopping-basket.service';
 
 @Component({
 	selector: 'app-dialog-payment',
@@ -8,9 +10,25 @@ import { TitleCasePipe } from '@angular/common';
 	styleUrls: ['./dialog-payment.component.scss'],
 })
 export class DialogPaymentComponent {
-	constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
+	constructor(
+		@Inject(MAT_DIALOG_DATA) public data: any,
+		public orderService: OrderHistoryService,
+		public dialog: MatDialog,
+		public cartService: ShoppingBasketService
+	) {}
 
 	getCardDetailsArray(details: any): any[] {
 		return Object.entries(details);
+	}
+
+	placeOrder() {
+		const order = {
+			paymentDetails: this.data,
+			products: this.cartService.products,
+		};
+
+		this.orderService.orders.push(order);
+		this.orderService.updateOrders();
+		this.dialog.closeAll();
 	}
 }
