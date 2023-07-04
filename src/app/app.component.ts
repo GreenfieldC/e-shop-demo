9c08091e7b5522ba7e8b164c4f1c61c95495131d
MoritzGeorgy95 <104759604+MoritzGeorgy95@users.noreply.github.com>
2023-07-04 10:44:35 +0200

    restructured app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,15 +25,27 @@ export class AppComponent {
 		if (authToken) {
 			const userData = JSON.parse(authToken);
 
-			this.cartService.cartReference = `user_${userData.id}/cart`;
-			this.cartService.currentlyLoggedInUser = userData.name;
-			this.cartService.getUserData();
+			this.getCart(userData);
 
-			this.orderService.orderReference = `user_${userData.id}/orders`;
-			this.orderService.currentlyLoggedInUser = userData.name;
-			this.orderService.getOrders();
+			this.getOrders(userData);
+
+			this.getAdresses(userData);
 		} else {
 			this.cartService.currentlyLoggedInUser = 'Guest';
 		}
 	}
+
+	getCart(userData: any) {
+		this.cartService.cartReference = `user_${userData.id}/cart`;
+		this.cartService.currentlyLoggedInUser = userData.name;
+		this.cartService.getUserData();
+	}
+
+	getOrders(userData: any) {
+		this.orderService.orderReference = `user_${userData.id}/orders`;
+		this.orderService.currentlyLoggedInUser = userData.name;
+		this.orderService.getOrders();
+	}
+
+	getAdresses(userData: any) {}
 }
