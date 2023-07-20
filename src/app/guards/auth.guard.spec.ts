47b6addc13169a11be895ab31a8eebb0a9cd162b
MoritzGeorgy95 <104759604+MoritzGeorgy95@users.noreply.github.com>
2023-07-20 14:33:14 +0200

    unit test adjustments

--- src/app/guards/auth.guard.spec.ts
+++ src/app/guards/auth.guard.spec.ts
@@ -1,16 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AuthGuard } from './auth.guard';
+import { authGuard } from './auth.guard'; // Corrected import statement
 
 describe('AuthGuard', () => {
-  let guard: AuthGuard;
+	let guard: any; // You can also replace 'any' with the appropriate type if known
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    guard = TestBed.inject(AuthGuard);
-  });
+	beforeEach(() => {
+		TestBed.configureTestingModule({});
+		guard = authGuard;
+	});
 
-  it('should be created', () => {
-    expect(guard).toBeTruthy();
-  });
+	it('should be created', () => {
+		expect(guard).toBeTruthy();
+	});
 });
--- src/app/user-management/google-signin.directive.spec.ts
+++ src/app/user-management/google-signin.directive.spec.ts
@@ -1,8 +1,19 @@
+import { TestBed } from '@angular/core/testing';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { GoogleSigninDirective } from './google-signin.directive';
 
 describe('GoogleSigninDirective', () => {
-  it('should create an instance', () => {
-    const directive = new GoogleSigninDirective();
-    expect(directive).toBeTruthy();
-  });
+	let directive: GoogleSigninDirective;
+
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			providers: [AngularFireAuth], // Provide the necessary dependency
+		});
+		const afAuth = TestBed.inject(AngularFireAuth); // Inject AngularFireAuth
+		directive = new GoogleSigninDirective(afAuth); // Pass the injected dependency
+	});
+
+	it('should create an instance', () => {
+		expect(directive).toBeTruthy();
+	});
 });
