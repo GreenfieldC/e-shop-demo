7831891a75271457a490ed7c7ec5e2fde028341f
MoritzGeorgy95 <104759604+MoritzGeorgy95@users.noreply.github.com>
2023-08-07 16:34:13 +0200

    added logic to prevent double reviews

--- src/app/order-management/orders-list/orders-list.component.ts
+++ src/app/order-management/orders-list/orders-list.component.ts
@@ -7,6 +7,7 @@ import { DialogProductDetailsComponent } from 'src/app/products/dialog-product-d
 import { HotToastService } from '@ngneat/hot-toast';
 import { DialogProblemComponent } from '../dialog-problem/dialog-problem.component';
 import { DialogProductReviewComponent } from '../dialog-product-review/dialog-product-review.component';
+import { ProductReviewService } from 'src/app/shared/services/product-review.service';
 
 @Component({
 	selector: 'app-orders-list',
@@ -18,7 +19,8 @@ export class OrdersListComponent {
 		public orderService: OrderHistoryService,
 		private dialog: MatDialog,
 		public cartService: ShoppingBasketService,
-		private toast: HotToastService
+		private toast: HotToastService,
+		private reviewService: ProductReviewService
 	) {
 		this.checkMobile();
 	}
@@ -63,11 +65,37 @@ export class OrdersListComponent {
 		this.dialog.open(DialogProblemComponent);
 	}
 
+	/**
+	 * check for existing review. User cant review product twice
+	 */
+	checkExistingReview(product: any) {
+		let checkPassed = true;
+
+		for (let i = 0; i < this.reviewService.reviews.length; i++) {
+			const review = this.reviewService.reviews[i];
+			if (
+				review.productId === product.id &&
+				review.username === this.cartService.currentlyLoggedInUser
+			) {
+				checkPassed = false;
+			}
+		}
+		return checkPassed;
+	}
+
+	/**
+	 * If product has not yet been reviewed, open dialog to write review
+	 * @param product
+	 */
 	openProductReview(product: any) {
-		this.dialog.open(DialogProductReviewComponent, {
-			height: 'fit-content',
-			width: '600px',
-			data: product,
-		});
+		if (this.checkExistingReview(product)) {
+			this.dialog.open(DialogProductReviewComponent, {
+				height: 'fit-content',
+				width: '600px',
+				data: product,
+			});
+		} else {
+			this.toast.error('You already wrote a review for this product!');
+		}
 	}
 }
